8eb82e5a048b3204dddc4ffffff7c61b0ef9971f
ilikecola <knight.lost.heaven@gmail.com>
2015-07-19 15:03:08 +0800

    Shadowsocks v2.6.11  manyuser 63b9ea130e
    First Commit

---
+++ Config.py
@@ -0,0 +1,12 @@
+#Config
+MYSQL_HOST = '133.130.49.68'
+MYSQL_PORT = 3306
+MYSQL_USER = 'ssdbuser'
+MYSQL_PASS = 'Remilia123456'
+MYSQL_DB = 'Shadowsocks'
+
+MANAGE_PASS = 'ssmanagePASS'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 22333
